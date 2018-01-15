d70bb9b74b6b5ab2fbb05078d9932a72cadbcbb7
spiedeman <spiedeman@gmail.com>
2018-01-15 13:03:04 +0800

    modify files

--- install.sh
+++ install.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
-cp .config/.bashrc ~/.bashrc
+echo -e "<< change source to Tsinghua"
+sudo -S cp /etc/apt/sources.list /etc/apt/sources.list.old << EOF
+xiaomei1102
+EOF
+sudo cp ./config/sources.list /etc/apt/sources.list
+sudo apt autoremove libreoffice-common unity-webapps-common
+echo "\n" | sudo -S add-apt-repository ppa:jonathonf/vim
+sudo apt update
+sudo apt upgrade
+sudo apt install vim-gtk3-py2
+vimpy2=`echo '1' | sudo -S update-alternatives --config vim | grep py2` 
+echo ${vimpy2%%/*} | sudo update-alternatives --config vim
+
+cp ./config/bashrc ~/.bashrc
 cp ./config/vimrc ~/.vimrc
 cp ./config/tmux.conf ~/.tmux.conf
 
