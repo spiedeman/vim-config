aa4ea597f99df0ad3f5f7fd711cc1e8c3e4f8ef7
spiedeman <spiedeman@gmail.com>
2018-01-27 17:12:39 +0800

    modify docker files

--- docker/install.sh
+++ docker/install.sh
@@ -23,3 +23,30 @@ sudo apt update
 
 # install the latest version of Docker CE
 sudo apt install docker-ce
+
+# create `docker` group 
+sudo groupadd docker
+
+# add user to the `docker` group
+sudo usermod -aG docker $USER
+
+# log out and log back so that the group membership is re-evaluated
+# verify that run `docker` commands without `sudo`
+docker run hello-world
+
+# This command download a test image and runs it in a container.
+# when the container runs, it prints an informational message and exits.
+
+## Configure Docker to start on boot
+# Most current Linux distributions (containing Ubuntu16.04 and higher) use
+# `systemd` to manage which services start when the system boots.
+sudo systemctl enable docker
+
+# To disable this behavior, use `disable` instead
+sudo systemctl disable docker
+
+# Docker is automatically configured to start on boot using `upstart`. To
+# disable this behavior, use the following command
+echo manual | sudo tee /etc/init/docker.override
+
+sudo chkconfig docker on
